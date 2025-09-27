19c49c4da20ba366d00d630a555cfc916ad419ef
Moreeh Ahmed <146959594+Moreeh97@users.noreply.github.com>
2025-09-27 17:10:54 +0300

    feat: Setup MongoDB connection, dotenv, and logging middleware

--- server.js
+++ server.js
@@ -1,6 +1,25 @@
 const express = require("express");
 const productsRoutes = require("./routes/productsRoutes");
 
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+
+dotenv.config();
+const DB_URI = process.env.DB_URI;
+
+mongoose.connect(DB_URI)
+  .then(() => console.log(" MongoDB connected"))
+  .catch(err => console.error(" MongoDB connection error:", err));
+
+app.use(morgan("dev"));
+
+app.use((req, res, next) => {
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+  next();
+});
+
+
 const app = express();
 
 app.use(express.json());
